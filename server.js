4d32361bd7c0c7a224fb944e8e6bf26c63b74a2f
Jake Peterson <39825740+jake-101@users.noreply.github.com>
2022-01-17 18:41:57 -0800

    switch request out for axios

--- server.js
+++ server.js
@@ -2,16 +2,18 @@ const jsonServer = require('json-server')
 const server = jsonServer.create()
 var db = {}
 const middlewares = jsonServer.defaults()
-var request = require('request')
-var opts = { url: `https://${process.env.VERCEL_URL}/db`, json: true }
-request(opts, function (error, response, body) {
-  if (error) throw error
-db = body
+const axios = require("axios");
+axios.get(`https://${process.env.VERCEL_URL}/db`).then((response) => {
+db = response
 const router = jsonServer.router(db)
 server.use(middlewares)
 server.use(router)
 server.listen(3000, () => {
-  console.log('JSON Server is running')
+  console.log('JSON Server is running', "API Key: ", process.env.PINBOARD_TOKEN, "URL: ", process.env.VERCEL_URL)
 })
-})
+}, (error) => {
+  console.log(error);
+});
+
+
 
